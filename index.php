4127eacf3bdcb96d789e1255a02d44528cf34f25
Shashwat Joshi <shashwat2002joshi@gmail.com>
2020-06-15 03:26:48 +0530

    second commit

--- index.php
+++ index.php
@@ -2,9 +2,12 @@ welcome to my web page
 <br />
 
 <pre>
+</html>
+<body>
 <?php
-
-print `ifconfig`;
-
+    print `ifconfig`;
+    echo "<img src = 'https://shashwat1234567.s3.ap-south-1.amazonaws.com/image.jpeg' />";
 ?>
+</body>
+</html>
 </pre>
